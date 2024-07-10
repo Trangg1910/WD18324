7403d715fed4940ad33b5847ef475b4123954d3b
Trangg1910 <nhtrang191004@gmail.com>
2024-07-10 14:03:18 +0700

    Lap1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\NhtController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +13,25 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+//Base url : http://127.0.0.1:8000/
 Route::get('/', function () {
-    return view('welcome');
+    echo "Trang chủ";
 });
+
+// Điêu hướng qua action của controller
+// php artisan make:controller Tên Controller
+
+Route::get('list-product', [ProductController::class, 'showProduct']);
+// Gửi dữ liệu qua controller
+// slug
+// http://127.0.0.1:8000/get-product/1
+Route::get('get-product/{id}/{name?}', [ProductController::class, 'getProduct']);
+
+// Params
+// http://127.0.0.1:8000/update-product?id=4&name=iphone
+Route::get('update-product', [ProductController::class, 'updateProduct']);
+
+
+// Lap 1
+Route::get('thongtinsv', [NhtController::class, 'showInfo']);
+
